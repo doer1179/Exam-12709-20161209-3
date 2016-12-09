3578fe057d240042046d5c71c62dec8da13f1043
yicheng <you@example.com>
2016-12-09 20:34:09 +0800

    修改了闰年判断的bug

--- src/main/java/com/wyc/Exam_12709_20161209_3/App.java
+++ src/main/java/com/wyc/Exam_12709_20161209_3/App.java
@@ -12,11 +12,10 @@ public class App
 		    System.out.println("请输入日期，以-隔开：");
 		    String[] str = scanner.next().split("-");//分割字符串
 		    int year = Integer.parseInt(str[0]);//拿出年份的字符串并转换为Int型
-		    if(year > 2 && year <= 12)
-		      b = true;
+		
 		    //判断是否为闰年
 		    if(year % 4 == 0 && year % 100 != 0 ||year % 400 == 0)
-		      leap = true;
+		      {leap = true;}
 		    int month =  Integer.parseInt(str[1]);//拿出月份的字符串并转换为Int型
 		    switch(month){
 		      case 2 : sum = 31;break;//计数器
@@ -33,8 +32,8 @@ public class App
 		    }
 		    int day =  Integer.parseInt(str[2]);//拿出日期的字符串并转换为Int型
 		    sum += day;
-		    if(leap && b)
-		      sum ++;
+		    if(leap )
+		      {++sum;}
 		    System.out.println("您输入的日期是该年第" + sum + "天");
 		  }
 }
